b98e835a0509ae933cbf7a65ba5b7ef4a449dc92
Senon Silvestre <senjaymac@gmail.com>
2022-07-18 07:43:00 +0800

    imports

--- VB - Resume Creator (Json)/Form1.vb
+++ VB - Resume Creator (Json)/Form1.vb
@@ -1,5 +1,12 @@
-﻿Public Class formMenu
+﻿Imports System.IO
+Imports Newtonsoft.Json
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports iTextSharp.text.pdf.draw
+Public Class formMenu
     Private Sub formMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+
 End Class
