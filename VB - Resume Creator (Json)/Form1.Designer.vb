64ef3c4e0890d05ebeb1fd12fdf138c009b4d227
Senon Silvestre <senjaymac@gmail.com>
2022-07-18 07:10:20 +0800

    Initial commit - renamed main form *disabled maximize icon *removed show icon

--- VB - Resume Creator (Json)/Form1.Designer.vb
+++ VB - Resume Creator (Json)/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class formMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(formMenu))
+        Me.SuspendLayout()
+        '
+        'formMenu
+        '
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.MaximizeBox = False
+        Me.Name = "formMenu"
+        Me.ShowIcon = False
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- VB - Resume Creator (Json)/Form1.vb
+++ VB - Resume Creator (Json)/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class Form1
+﻿Public Class formMenu
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
