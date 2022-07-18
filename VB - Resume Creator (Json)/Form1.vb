6ad2cb2dc888931d6eeea659533c0cf14b2a2753
Senon Silvestre <senjaymac@gmail.com>
2022-07-18 08:21:22 +0800

    Added Json File
    referrence added seems working

--- VB - Resume Creator (Json)/Form1.vb
+++ VB - Resume Creator (Json)/Form1.vb
@@ -4,9 +4,12 @@ Imports iTextSharp.text
 Imports iTextSharp.text.pdf
 Imports iTextSharp.text.pdf.draw
 Public Class formMenu
-    Private Sub formMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim jsonpath As String = "C:\Users\Element 54\Documents\Repos\Resume Creator\VB - Resume Creator (Json)\VB - Resume Creator (Json)\ resumeInfo.json"
+        Dim openfile As String = File.ReadAllText(jsonpath)
+        Dim resumeout As JsonReader = JsonConvert.DeserializeObject(Of JsonReader)(openfile)
+        Dim pdfcreator As Document = New Document()
+        PdfWriter.GetInstance(pdfcreator, New FileStream("C:\Users\Element 54\Documents\Repos\Cl\json\Resume Creator VB\ Testing.pdf", FileMode.Create))
     End Sub
-
-
 End Class
--- VB - Resume Creator (Json)/My Project/Application.Designer.vb
+++ VB - Resume Creator (Json)/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB___Resume_Creator__Json_.Form1
+            Me.MainForm = Global.VB___Resume_Creator__Json_.formMenu
         End Sub
     End Class
 End Namespace
